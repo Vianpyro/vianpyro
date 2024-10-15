5b2f6abfb4b453ffbec3832b1df93bfd03d29263
Vianpyro <vianney@veremme.org>
2024-10-14 22:42:54 -0400

    Change to kebab-case

--- src/javascript/github_repos.js
+++ src/javascript/github_repos.js
@@ -1,7 +1,7 @@
 // Load user's GitHub repositories
-async function load_user_github_data(username) {
+async function loadGithubUserData(username) {
   // Clear the page
-  document.getElementById('github_repos').innerHTML = '';
+  document.getElementById('github-repos').innerHTML = '';
 
 
   try {
@@ -11,17 +11,17 @@ async function load_user_github_data(username) {
     // Load the title and image
     response = await fetch(`https://api.github.com/user/${json[0].owner.id}`);
     var data = await response.json();
-    const user_name = data.name != null ? data.name : username;
-    document.getElementById('github_username').innerHTML = user_name;
-    document.getElementById('github_user_url').href = `https://github.com/${username}`;
-    document.getElementById('github_user_profile_picture').src = `https://avatars3.githubusercontent.com/u/${json[0].owner.id}`;
-    document.title = user_name;
+    const userName = data.name != null ? data.name : username;
+    document.getElementById('github-username').innerHTML = userName;
+    document.getElementById('github-user-url').href = `https://github.com/${username}`;
+    document.getElementById('github-user-profile-picture').src = `https://avatars3.githubusercontent.com/u/${json[0].owner.id}`;
+    document.title = userName;
 
     // Load the bio
     response = await fetch(`https://api.github.com/users/${username}`);
     data = await response.json();
-    const user_bio = data.bio != null ? data.bio : '';
-    document.getElementById('github_user_bio').innerHTML = `"${user_bio}"`;
+    const userBio = data.bio != null ? data.bio : '';
+    document.getElementById('github-user-bio').innerHTML = `"${userBio}"`;
 
     // Load the colors
     response = await fetch('https://raw.githubusercontent.com/ozh/github-colors/master/colors.json');
@@ -29,18 +29,18 @@ async function load_user_github_data(username) {
 
     // Load the page
     json.forEach(element => {
-      const { name, fork, description, language, html_url, homepage } = element;
+      const { name, fork, description, language, htmlUrl, homepage } = element;
       const forked = fork ? `<span>(<b>Forked</b>)</span>` : "";
       const lang = language ? `<b style="color: ${colors[language].color};">•</b> ${language}` : "";
       const desc = description != null ? description : "";
 
-      document.getElementById('github_repos').innerHTML += `
-      <div class="github_repo">
-        <a href="${html_url}" target = "_blank" id="js-${name}" class="repo_detail">
-          <span class="repo_name">${name.replace(/-/g, " ").replace(/   /g, " - ")} ${forked}</span>
+      document.getElementById('github-repos').innerHTML += `
+      <div class="github-repo">
+        <a href="${htmlUrl}" target = "_blank" id="js-${name}" class="repo-detail">
+          <span class="repo-name">${name.replace(/-/g, " ").replace(/   /g, " - ")} ${forked}</span>
         </a>
-        <span class="repo_detail">${desc}</span>
-        <span class="repo_detail">${lang}</span>
+        <span class="repo-detail">${desc}</span>
+        <span class="repo-detail">${lang}</span>
         </div>
         `;
       if (homepage && name != username) { document.getElementById(`js-${name}`).href = homepage };
@@ -52,23 +52,23 @@ async function load_user_github_data(username) {
     link.href = `https://avatars3.githubusercontent.com/u/${json[0].owner.id}`;
     document.getElementsByTagName('head')[0].appendChild(link);
   } catch (err) {
-    document.getElementById('github_user_profile_picture').src = "./src/img/octocat.png";
-    document.getElementById('github_username').innerHTML = username;
-    document.getElementById('github_repos').innerHTML = 'This user seems not to have any public repository (yet).';
-    document.getElementById('github_repos').style.textAlign = 'center'
-    document.getElementById('github_user_bio').innerHTML = "";
+    document.getElementById('github-user-profile-picture').src = "./src/img/octocat.png";
+    document.getElementById('github-username').innerHTML = username;
+    document.getElementById('github-repos').innerHTML = 'This user seems not to have any public repository (yet).';
+    document.getElementById('github-repos').style.textAlign = 'center'
+    document.getElementById('github-user-bio').innerHTML = "";
     document.title = username;
     console.log(err);
   }
-  document.getElementById('github_user_profile_picture').alt = `${username}'s profile picture`;
-  document.getElementById('github_username_url').href = `https://github.com/${username}`;
+  document.getElementById('github-user-profile-picture').alt = `${username}'s profile picture`;
+  document.getElementById('github-username-url').href = `https://github.com/${username}`;
 }
 
 // Wait for the DOM to be loaded before loading the user's profile
 window.addEventListener("DOMContentLoaded", () => {
-  load_user_github_data('Vianpyro');
+  loadGithubUserData('Vianpyro');
 });
 
-async function secret_command_load_username(new_username) {
-  load_user_github_data(new_username);
+async function secretCommandLoadUsername(newUsername) {
+  loadGithubUserData(newUsername);
 }
